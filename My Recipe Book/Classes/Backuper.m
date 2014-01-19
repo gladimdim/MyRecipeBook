8776c39f686df6ce273684c22aa935f1698d8a9c
Dmytro Gladkyi <gladimdim@gmail.com>
2014-01-19 16:06:52 +0200

    Cleaned comments.

--- My Recipe Book/Classes/Backuper.m
+++ My Recipe Book/Classes/Backuper.m
@@ -17,29 +17,10 @@
 +(void) backUpFileToLocalDrive:(FoodTypesDocument *)fileToBackup {
     if ([CloudManager sharedManager].iCloudAvailable) {
         FoodTypesDocument *doc = [[FoodTypesDocument alloc] initWithFileURL:[[CloudManager sharedManager] localDocumentURL]];
-       /* [doc openWithCompletionHandler:^(BOOL success) {
-            if (success) {
-                //check if food type exists in local copy. if yes - scan recipes and add if necessary
-                for (FoodType *cloudFoodType in fileToBackup.recipeBook.arrayOfFoodTypes) {
-                    FoodType *foundLocalType = [doc.recipeBook containsFoodTypeByName:cloudFoodType.name];
-                    if (foundLocalType) {
-                        for (Recipe *cloudRecipe in cloudFoodType.arrayOfRecipes) {
-                            if (![doc.recipeBook findRecipeByName:cloudRecipe.name]) {
-                                [foundLocalType.arrayOfRecipes addObject:cloudRecipe];
-                            }
-                        }
-                    }
-                    [doc.recipeBook.arrayOfFoodTypes addObject:cloudFoodType];
-                }
-            }
-            else {*/
-                doc.recipeBook = fileToBackup.recipeBook;
+                 doc.recipeBook = fileToBackup.recipeBook;
                 [doc saveToURL:[[CloudManager sharedManager] localDocumentURL] forSaveOperation:UIDocumentSaveForCreating completionHandler:^(BOOL success) {
                     NSLog(@"Backup saved: %@", success ? @"YES" : @"NO");
                 }];
-/*
-            }
-        }];*/
     }
 }
 @end
